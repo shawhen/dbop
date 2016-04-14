1b0fda311321e38b263517d5fcf27276bbc2e39c
shaw <shawhen2012@hotmail.com>
2016-04-14 16:19:45 +0800

    [update] make logs dir

--- configs/logs.py
+++ configs/logs.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 # @author: zig(shawhen2012@hotmail.com)
 
+import os
 import logbook
 
+try:
+    os.makedirs("logs")
+except FileExistsError:
+    pass
 logpath = "logs/log.log"
 loglevel = logbook.TRACE
